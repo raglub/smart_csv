499e8dfa1c3741d78b570f567b7b59ac31f49fae
Michał Szyma <devszyma@gmail.com>
2013-11-02 18:35:54 +0100

    Added #not into CSV::Table class

--- lib/smart_csv/version.rb
+++ lib/smart_csv/version.rb
@@ -1,3 +1,3 @@
 module SmartCSV
-  VERSION = "0.0.8"
+  VERSION = "0.0.9"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,9 @@ require 'bundler/setup'
 
 require 'smart_csv'
 
+require 'coveralls'
+Coveralls.wear!
+
 RSpec.configure do |config|
   # some (optional) config here
 end
